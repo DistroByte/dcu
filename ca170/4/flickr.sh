e105a4702316291568a9af630289ab17e846b8ca
James Hackett <jamesthackett1@gmail.com>
2022-03-16 04:35:34 +0000

    Update flickr.sh

--- ca170/4/flickr.sh
+++ ca170/4/flickr.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-img=$1
-
-wget -q -O - "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=$img" | grep "description" | tr -d '\\' | grep -o ' <p>.*</p>' > $HOME/flickr.html
+wget "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=$img" > $HOME/flickr.html
