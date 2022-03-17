852d9b5e41b9a3b8a2f4443d8c988dd3c0577c1e
James Hackett <jamesthackett1@gmail.com>
2022-03-17 00:32:21 +0000

    Adds CA170

--- ca170/4/flickr.sh
+++ ca170/4/flickr.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-wget "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=$img" > $HOME/flickr.html
+img=$1
+
+wget -q -O - "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=$img" | grep "description" | tr -d '\\' | grep -o ' <p>.*</p>' > $HOME/flickr.html
